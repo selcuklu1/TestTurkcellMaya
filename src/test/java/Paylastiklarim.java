f0004291f44633ad71579c4ad5e883bb94ebff08
Ilyas Bayraktar <bayilyas@gmail.com>
2017-11-13 14:30:20 +0300

    Test commet from idea

--- src/test/java/Paylastiklarim.java
+++ src/test/java/Paylastiklarim.java
@@ -40,7 +40,6 @@ public class Paylastiklarim extends BaseTest {
 
     @Test()
     public void TC1() {
-        System.out.println("Test11");
-        System.out.println("Test2");
+        System.out.println("Ilyas");
     }
 }
