06e138b3225ced23b5acfd2be347b1660d6a00c8
Ilyas Bayraktar <bayilyas@gmail.com>
2017-11-20 16:20:38 +0300

    Evrak Oluştur Page çalışması

--- src/test/java/EvrakOlusturTest.java
+++ src/test/java/EvrakOlusturTest.java
@@ -3,7 +3,6 @@ import common.BaseTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import pages.BasePage;
 import pages.EvrakOlusturPage;
 import pages.EvrakOlusturPage.BilgilerTab;
 import pages.pageComponents.belgenetElements.BelgentCondition;
@@ -15,12 +14,12 @@ public class EvrakOlusturTest extends BaseTest {
 
     @BeforeClass
     public void setUp() throws Exception {
-        new BasePage().loginPage().login();
+        login();
         evrakOlusturPage = new EvrakOlusturPage();
         evrakOlusturPage.open();
 
         tabBilgiler = evrakOlusturPage.new BilgilerTab();
-        tabBilgiler.open();
+        tabBilgiler.openPage();
         tabBilgiler.konuKoduSec("ss").getTxtKonu();
 
     }
