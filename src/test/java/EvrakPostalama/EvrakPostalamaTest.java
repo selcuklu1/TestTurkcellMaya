84e849c8c7ea9591cc53ff85d3c4ca2620abb98a
hakan-guner <hakan.guner@optiim.com>
2017-11-23 15:02:53 +0300

    EvrakPostalama class created

---
+++ src/test/java/EvrakPostalama/EvrakPostalamaTest.java
@@ -0,0 +1,17 @@
+package EvrakPostalama;
+
+/****************************************************
+ * Yazan: Hakan Güner
+ * Tarih: 2017-11-23
+ * Proje: Türksat Functional Test Automation
+ * Class: "Evrak Postalama" konulu senaryoları içerir
+ ****************************************************/
+
+import common.BaseTest;
+
+public class EvrakPostalamaTest extends BaseTest {
+
+
+
+
+}
