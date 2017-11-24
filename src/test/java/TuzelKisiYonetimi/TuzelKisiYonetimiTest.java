457b883ce8f75ac4330d5bec800b72d0070c9b60
Sezai Çelik <sezai.celik@optiim.com>
2017-11-24 17:07:06 +0300

    TC1124 Tüzel Kişi Yönetimi

--- src/test/java/TuzelKisiYonetimi/TuzelKisiYonetimiTest.java
+++ src/test/java/TuzelKisiYonetimi/TuzelKisiYonetimiTest.java
@@ -3,7 +3,6 @@ package TuzelKisiYonetimi;
 import common.BaseTest;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import io.qameta.allure.Step;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.ustMenuPages.*;
@@ -14,7 +13,7 @@ import pages.ustMenuPages.*;
  * Class: "Tüzel Kişi Yönetimi" konulu senaryoları içerir
  * Yazan: Sezai Çelik 
  ****************************************************/
-public class TuzelKisiYonetimiTest extends BaseTest{
+public class TuzelKisiYonetimiTest extends BaseTest {
 
     TuzelKisiYonetimiPage tuzelKisiYonetimiPage;
 
@@ -28,9 +27,44 @@ public class TuzelKisiYonetimiTest extends BaseTest{
     @Test(enabled = true, description = "TC1124: Yeni tüzel kişi kayıt ve ekranlardan kontrolleri")
     public void TC1124() {
 
+        String vergiNo = createRandomNumber(7);
+        String ad = "Sezai Çelik Holding";
+        String kisaAd = "SCH";
+        String onEk = "Muh";
+        String unvan = "Mühendis";
+        String adres = "İstiklal Caddesi";
+        String ulke = "TÜRKİYE";
+        String il = "İst";
+        String ilce = "Bey";
+        String eposta = "scholding@turksat.com.tr";
+        String webAdres = "www.scholding.com";
+        String telNo = "5391111111";
+        String faksNo = "2121111111";
+        String basariMesaji = "İşlem başarılıdır!";
+
         tuzelKisiYonetimiPage
                 .openPage()
-                .tuzeKisiEkle();
+                .yeniTuzelKisiEkle()
+                .tuzelKisiTipiSec("12729")
+                .vergiNoDoldur(vergiNo)
+                .adDoldur(ad)
+                .kisaAdDoldur(kisaAd)
+                .yeniIletisimEkle()
+
+                .mobilTelNoDoldur(telNo)
+                .telNoDoldur(telNo)
+                .faks1NoDoldur(faksNo)
+                .faks2NoDoldur(faksNo)
+                .adresDoldur(adres)
+                .ulkeSec(ulke)
+                .ilSec(il)
+                .ilceSec(ilce)
+                .ePostaDoldur(eposta)
+                .webAdresDoldur(webAdres)
+                .iletisimBilgisiKaydet()
+
+                .tuzelKisiKaydet()
+                .islemMesaji().basariliOlmali(basariMesaji);
 
     }
 }
