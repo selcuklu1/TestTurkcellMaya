d6d466f0bfe32c71a24826b2d46fe8e45e16bbff
Sezai Çelik <sezai.celik@optiim.com>
2017-11-28 10:58:50 +0300

    TC1460 Tüzel kişi eklendi.

--- src/main/java/common/BaseTest.java
+++ src/main/java/common/BaseTest.java
@@ -28,28 +28,22 @@ public class BaseTest extends BaseLibrary {
 
         //region Selenide Driver Configuration
         Configuration.baseUrl = belgenetURL;
-       Configuration.browser = "drivers.Firefox";
+        //    Configuration.browser = "drivers.Firefox";
 //        Configuration.remote = "http://0.0.0.0:32768/wd/hub";
-//        Configuration.browser = "chrome";
- //       Configuration.browser = "marionette";
- //       Configuration.browser = "chrome";
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
+        //       Configuration.browser = "marionette";
+        //       Configuration.browser = "chrome";
         //Configuration.browser = "marionette";
-<<<<<<< HEAD
         //Configuration.browser = "chrome";
-=======
-     //   Configuration.browser = "chrome";
->>>>>>> b8bcd2bf578d5da7e45cfd02605b7550f193e5f2
-        //Configuration.browser = "marionette";
-
-        Configuration.browser = "drivers.Firefox";
-        //"org.openqa.selenium.Firefox.FirefoxDriver";
+        //   Configuration.browser = "chrome";
         Configuration.reportsFolder = "test-result/reports";
         Configuration.screenshots = false;
         Configuration.savePageSource = false;
         Configuration.collectionsTimeout = 20000;
-        Configuration.timeout = 20000;
+        Configuration.timeout = 30000;
         Configuration.holdBrowserOpen = true;
-//        Configuration.startMaximized = true;
+
 //        Configuration.headless = true;
 //        Configuration.browserSize = "1024x600";
         //endregion
