7da281851ed82feb27aaeab6d602eb50427037fe
Ilyas Bayraktar <bayilyas@gmail.com>
2017-12-01 16:11:36 +0300

    Hub ayarı

--- src/main/java/common/BaseTest.java
+++ src/main/java/common/BaseTest.java
@@ -30,9 +30,10 @@ public class BaseTest extends BaseLibrary{
         //region Selenide Driver Configuration
         Configuration.baseUrl = belgenetURL;
         //Configuration.browser = "drivers.Firefox";
-        //Configuration.browser = "chrome";
-        Configuration.browser = "marionette";
-        //Configuration.remote = "http://0.0.0.0:32768/wd/hub";
+//        Configuration.browser = "chrome";
+//        Configuration.browser = "marionette";
+
+        Configuration.remote = "http://10.101.20.153:4444/wd/hub";
         Configuration.reportsFolder = "test-result/reports";
         Configuration.screenshots = false;
         Configuration.savePageSource = false;
