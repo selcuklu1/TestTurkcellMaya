bd44f66d6866d97b15ac9ab15c9dfff67545f25a
emre sencan <emre.sencan@optiim.com>
2017-12-04 17:18:45 +0300

    TC1340 yazıldı

---
+++ src/main/java/pages/solMenuPages/KaydedilenGidenEvraklarPage.java
@@ -0,0 +1,10 @@
+package pages.solMenuPages;
+
+/****************************************************
+ * Tarih: 2017-12-22
+ * Proje: Türksat Functional Test Automation
+ * Class: "Evrak kontroller" konulu senaryoları içerir
+ * Yazan: Emre Sencan
+ ****************************************************/
+public class KaydedilenGidenEvraklarPage {
+}
