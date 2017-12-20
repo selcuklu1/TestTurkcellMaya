abe7ff783601148b56b7e1b18eb3c627d5347c44
Sezai Çelik <sezai.celik@optiim.com>
2017-12-20 13:38:38 +0300

    path sorunu

--- src/main/java/common/BaseTest.java
+++ src/main/java/common/BaseTest.java
@@ -43,7 +43,7 @@ public class BaseTest extends BaseLibrary {
         //Configuration.browser = "marionette";
 
 
-        Configuration.remote = "http://192.168.1.3:6570/wd/hub";
+        //Configuration.remote = "http://192.168.1.3:6570/wd/hub";
         //Configuration.remote = "http://10.101.20.153:4444/wd/hub";
         Configuration.reportsFolder = "test-result/reports";
         Configuration.screenshots = false;
