1530f65d5769714b63481d827e5e6600d049ec3c
Sezai Çelik <sezai.celik@optiim.com>
2017-12-22 12:44:36 +0300

    commit

--- src/test/java/tests/GelenEvrakiCevapliKapat/GelenEvrakiCevapliKapatTest.java
+++ src/test/java/tests/GelenEvrakiCevapliKapat/GelenEvrakiCevapliKapatTest.java
@@ -1,10 +1,19 @@
 package tests.GelenEvrakiCevapliKapat;
 
+import common.BaseTest;
+import org.testng.annotations.BeforeMethod;
+
 /****************************************************
  * Tarih: 2017-12-22
  * Proje: Türksat Functional Test Automation
- * Class: "Evrak kontroller" konulu senaryoları içerir
+ * Class: "Gelen Evrakı Cevaplı Kapat" konulu senaryoları içerir
  * Yazan: Sezai Çelik 
  ****************************************************/
-public class GelenEvrakiCevapliKapatTest {
+public class GelenEvrakiCevapliKapatTest extends BaseTest{
+
+    @BeforeMethod
+    public void loginBeforeTests() {
+        login();
+    }
+
 }
