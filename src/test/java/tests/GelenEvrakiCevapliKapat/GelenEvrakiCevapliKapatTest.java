9946da4e84cf017fd64b6ed38b914c3413072650
Sezai Çelik <sezai.celik@optiim.com>
2017-12-22 12:43:50 +0300

    commit

---
+++ src/test/java/tests/GelenEvrakiCevapliKapat/GelenEvrakiCevapliKapatTest.java
@@ -0,0 +1,10 @@
+package tests.GelenEvrakiCevapliKapat;
+
+/****************************************************
+ * Tarih: 2017-12-22
+ * Proje: Türksat Functional Test Automation
+ * Class: "Evrak kontroller" konulu senaryoları içerir
+ * Yazan: Sezai Çelik 
+ ****************************************************/
+public class GelenEvrakiCevapliKapatTest {
+}
