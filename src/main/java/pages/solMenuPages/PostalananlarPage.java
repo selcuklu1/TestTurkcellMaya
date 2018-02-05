173a165785f374f66fb9cbd7e86165a3c992695c
hakan <hakan.guner>
2018-02-05 03:09:07 +0300

    fix update

--- src/main/java/pages/solMenuPages/PostalananlarPage.java
+++ src/main/java/pages/solMenuPages/PostalananlarPage.java
@@ -61,6 +61,7 @@ public class PostalananlarPage extends MainPage {
     SelenideElement tabIcerikKapatmaOnay = $(By.id("kapatButton"));
     SelenideElement btnIcerikPostaDetayi = $x("//*[@id='inboxItemInfoForm:dialogTabMenuRight:uiRepeat:4:cmdbutton']/span[1]");
     //
+    SelenideElement popupEvrakYazdirma = $x("//*[@id='postaDetayYazdirForm:dtPostaEvrakUstVeri:0:evrakDetayiViewDialogYazdir']");
     SelenideElement tuzelKisiGuncelle = $x("//*[@id='mainPreviewForm:postalananDataGrid']/tbody/tr/td/div/table/tbody/tr[4]/td[8]/div/button[1]");
     //
     SelenideElement kullaniciGuncelle = $x("//*[@id='mainPreviewForm:postalananDataGrid']/tbody/tr/td/div/table/tbody/tr[5]/td[8]/div/button[1]");
@@ -302,14 +303,14 @@ public class PostalananlarPage extends MainPage {
         return this;
     }
 
-    @Step("Icerik içinde Ilgileri Tabına tıklama")
+    @Step("Icerik içinde Ilgileri Tabına tıklama ve kontrol (kontrol için 500ms delay bulunmakta)")
     public PostalananlarPage btnIcerikIlgileriTab() throws InterruptedException {
         btnIlgileriIcerik.click();
         Thread.sleep(500);
         return this;
     }
 
-    @Step("Icerik içinde Ekleri Tabına tıklama")
+    @Step("Icerik içinde Ekleri Tabına tıklama ve kontrolü")
     public PostalananlarPage btnIcerikEkleriTab() {
         btnEkleriIcerik.click();
         return this;
@@ -395,6 +396,17 @@ public class PostalananlarPage extends MainPage {
         return this;
     }
 
+    @Step("Evrak Yazdır Popup içi Pdf yazdırma ve kontrol ve popup kapatma")
+    public PostalananlarPage popupYazpdfkontrolveKapatma () {
+        popupEvrakYazdirma.click();
+        SelenideElement ustyazi = $x("//*[@id='postaDetayYazdirForm:dtPostaEvrakUstVeri_data']/tr/td[2]/div");
+        String pdf = ustyazi.getAttribute("innerText");
+        System.out.println(pdf);
+        SelenideElement popupkapat = $x("//*[@id='postaDetayYazdirForm:dlgPostaDetayYazdir']/div[1]/a/span");
+        popupkapat.click();
+        return this;
+
+    }
     @Step("Etiket Bastir ve kontrol")
     public PostalananlarPage etiketBastir() {
 
