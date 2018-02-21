bd6bb84edc3ef2990246a269808a0ee20e7cd39b
Ilyas Bayraktar <ilyas.bayraktar@optiim.com>
2018-02-21 12:17:10 +0300

    comm

--- src/main/java/common/ReusableSteps.java
+++ src/main/java/common/ReusableSteps.java
@@ -1,6 +1,7 @@
 package common;
 
 import data.TestData;
+import data.User;
 import io.qameta.allure.Step;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,8 +22,6 @@ import static sun.security.jgss.GSSUtil.login;
 
 public class ReusableSteps extends BaseLibrary{
 
-
-
     @Step("Beklemeye Alınanlar evrak Oluştur.")
     public void beklemeyeAlinanlarEvrakOlustur(String konu,String geregiSecimTipi, String geregi, String onayAkisiKullanici1Turu,String onayAkisiKullanici2,String onayAkisiKullaniciBirimi,String onayAkisiKullanici2turu,String user,String password) {
 
@@ -33,6 +32,14 @@ public class ReusableSteps extends BaseLibrary{
         evrakOlusturPage.evrakOlusturParafla(konu,geregiSecimTipi,geregi,onayAkisiKullanici1Turu,onayAkisiKullanici2,onayAkisiKullaniciBirimi,onayAkisiKullanici2turu);
         loginPage.login(user,password);
         imzaBekleyenler.imzaBekleyenlerEvrakSecBeklemeyeAl(konu);
+    }
+
+    @Step("Beklemeye Alınanlar evrak Oluştur.")
+    public void beklemeyeAlinanlarEvrakOlustur(String konu, String geregiSecimTipi, String geregi, User evrakOlusturan, User imzaci) {
+        new EvrakOlusturPage()
+                .evrakOlusturParafla(konu,geregiSecimTipi,geregi,"Parafla",imzaci.getFullname(), imzaci.getBirimAdi(), "İzmala");
+        new LoginPage().login(imzaci);
+        new ImzaBekleyenlerPage().imzaBekleyenlerEvrakSecBeklemeyeAl(konu);
     }
 
 }
