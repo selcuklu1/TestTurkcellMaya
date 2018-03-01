a1c5d6ec59c5f513614a3646f226479f63b0b58b
Can Şeker <can.seker@optiim.com>
2018-03-01 09:37:41 +0300

    Evrak havale İslemleri

--- src/test/java/tests/EvrakHavaleIslemleri/EvrakHavaleIslemleriTest.java
+++ src/test/java/tests/EvrakHavaleIslemleri/EvrakHavaleIslemleriTest.java
@@ -4,22 +4,28 @@ import common.BaseTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import pages.solMenuPages.BirimHavaleEdilenlerPage;
+import pages.solMenuPages.KaydedilenGelenEvraklarPage;
 import pages.solMenuPages.TeslimAlinmayiBekleyenlerPage;
 import pages.ustMenuPages.BakimaAlPage;
 import pages.ustMenuPages.GelenEvrakKayitPage;
 
-import static data.TestData.passwordZTEKIN;
-import static data.TestData.usernameZTEKIN;
+import static data.TestData.*;
 
 
 public class EvrakHavaleIslemleriTest extends BaseTest {
 
     GelenEvrakKayitPage gelenEvrakKayitPage;
     TeslimAlinmayiBekleyenlerPage teslimAlinmayiBekleyenlerPage;
+    KaydedilenGelenEvraklarPage kaydedilenGelenEvraklarPage;
+    BirimHavaleEdilenlerPage birimHavaleEdilenlerPage;
+
     @BeforeMethod
     public void loginBeforeTests() {
+        kaydedilenGelenEvraklarPage = new KaydedilenGelenEvraklarPage();
         gelenEvrakKayitPage = new GelenEvrakKayitPage();
         teslimAlinmayiBekleyenlerPage = new TeslimAlinmayiBekleyenlerPage();
+        birimHavaleEdilenlerPage = new BirimHavaleEdilenlerPage();
     }
 
     @Test(enabled = true, description = "2295: Toplu havale ekranı alan kontrolü")
@@ -35,9 +41,44 @@ public class EvrakHavaleIslemleriTest extends BaseTest {
         gelenEvrakKayitPage.gelenEvrakKayitKullaniciHavaleEt(konuKodu,kurum,kullanici);
 
         gelenEvrakKayitPage
-                .evrakOlusturSayfayiKapat();
+                .evrakOlusturSayfaKapat();
 
         gelenEvrakKayitPage.gelenEvrakKayitBirimHavaleEt(konuKodu2,kurum,"Yazılım Geliştirme Direktörlüğü");
+    }
+
+
+    @Test(enabled = true, description = "TS2293: Görüntülenen evrakın birim havale edilenler listesinden geri çekilemediğinin kontrolü")
+    public void TS2293() {
+        String basariMesaji = "İşlem başarılıdır!";
+        String konuKodu = "TS2293-"+createRandomNumber(15);
+        String kurum = "BÜYÜK HARFLERLE KURUM";
+        String kullanici = "Zübeyde Tekin";
+        String birim = "YAZILIM GELİŞTİRME DİREKTÖRLÜĞÜ";
+
+        login(usernameZTEKIN,passwordZTEKIN);
+
+        gelenEvrakKayitPage.gelenEvrakKayitKaydedilenGelenEvraklarEvrakOlustur(konuKodu,kurum);
+
+        kaydedilenGelenEvraklarPage
+                .openPage()
+                .evrakNoIleEvrakSec(konuKodu)
+                .evrakSecHavaleYap()
+                .havaleYapBirimDoldur(birim)
+                .havaleYapGonder()
+                .islemMesaji().basariliOlmali(basariMesaji);
+
+        login(usernameMBOZDEMIR,passwordMBOZDEMIR);
+
+        teslimAlinmayiBekleyenlerPage
+                .openPage()
+                .evrakNoIleEvrakSec(konuKodu);
+
+        login(usernameZTEKIN,passwordZTEKIN);
+
+        birimHavaleEdilenlerPage
+                .openPage()
+                .evrakNoIleTablodanEvrakSecme(konuKodu);
+
     }
 
 }
