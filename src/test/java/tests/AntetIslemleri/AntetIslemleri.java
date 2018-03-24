a44f79ce145ebc6fe57f6f9a3c6d04b9d46b7aeb
Serdar Kayis <serdar.kayis@optiim.com>
2018-03-24 22:12:38 +0300

    antet fix

--- src/test/java/tests/AntetIslemleri/AntetIslemleri.java
+++ src/test/java/tests/AntetIslemleri/AntetIslemleri.java
@@ -26,8 +26,9 @@ import static com.codeborne.selenide.Condition.text;
 @Feature("Antet Islemleri")
 public class AntetIslemleri extends BaseTest {
 
-    public static final User ztekin = new User("ztekin", "123", "Zübeyde TEKİN", "GENEL MÜDÜRLÜK MAKAMI");
+    public static final User ztekinEnUst = new User("ztekin", "123", "Zübeyde TEKİN", "GENEL MÜDÜRLÜK MAKAMI");
     public static final User gsahinGuncel = new User("gsahin", "123", "Gökçe ŞAHİN", "Antet Güncel Birim");
+    public static final User ztekinGuncel = new User("ztekin", "123", "Zübeyde TEKİN", "Antet Güncel Birim");
 
     BirimYonetimiPage birimYonetimiPage;
     EvrakOlusturPage evrakOlusturPage;
@@ -50,7 +51,7 @@ public class AntetIslemleri extends BaseTest {
 
     @BeforeMethod
     public void loginBeforeTests() {
-        login(ztekin);
+        login(ztekinEnUst);
         birimYonetimiPage = new BirimYonetimiPage();
         evrakOlusturPage = new EvrakOlusturPage();
         editor = new TextEditor();
@@ -94,9 +95,9 @@ public class AntetIslemleri extends BaseTest {
         String editorIcerik = "Bu bir deneme mesajıdır. Lütfen dikkate almayınız.";
 //        String basariMesaji = "İşlem başarılıdır!";
         String user1 = "Gökçe ŞAHİN";
-//        String user2 = "Zübeyde TEKİN";
+        String user2 = "Zübeyde TEKİN";
 //        String user3 = "Yasemin Çakıl AKYOL";
-//        String details = "BHUPGMY";
+        String details = "Antet Üst Birim";
 //        String sayfa1 = "Evrak Oluştur";
 //        String evrakGuncellendiImzalanamazUyari = "Evrakınız güncellendiği için imzalanamaz! Evrakın iade edilmesi gerekmektedir.";
 //        String evrakİmzaUyari = "Sayısal imza ile imzaladığınız belge 5070 sayılı kanun kapsamına girmemektedir.";
@@ -104,6 +105,8 @@ public class AntetIslemleri extends BaseTest {
 //
 //        String secenek1 = "İade Et";
 //        String secenek2 = "İmzala ve devam et (Önceki kullanıcıları akıştan çıkartarak)";
+        String pathToFileText = getUploadPath() + "test.txt";
+        String fileName = "test.txt";
 
         testStatus(testid, "PreCondition");
         birimYonetimiPage
@@ -126,7 +129,8 @@ public class AntetIslemleri extends BaseTest {
                 .antetTipiSec("Antet Yok");
 
         testStatus(testid, "Test Başladı");
-        login(gsahinGuncel);
+        login(ztekinGuncel);
+
         evrakOlusturPage
                 .openPage()
                 .bilgilerTabiAc()
@@ -144,15 +148,13 @@ public class AntetIslemleri extends BaseTest {
                 .geregiSecimTipiKontrol("Kurum")
                 .geregiDoldur(geregiKurum,"Kurum")
                 .geregiKontrol(geregiKurum)
+                .dagitimiEkYapSec(true)
                 .onayAkisiEkle()
-                .onayAkisiKullaniciKontrolu(user1 , "Paraflama");
-//                .onayAkisiKullaniciEkle(user2,details)
-//                .onayAkisiKullaniciTipiSec(user2,"İmzalama")
-//                .onayAkisiKullaniciKontrolu(user2 , "İmzalama")
-//                .onayAkisiKullaniciEkle(user3,details)
-//                .onayAkisiKullaniciTipiSec(user3,"İmzalama")
-//                .onayAkisiKullaniciKontrolu(user3 , "İmzalama")
-//                .kullan()
+                .onayAkisiKullaniciKontrolu(user2 , "Paraflama")
+                .onayAkisiKullaniciEkle(user1,details)
+                .onayAkisiKullaniciTipiSec(user1,"İmzalama")
+                .onayAkisiKullaniciKontrolu(user1 , "İmzalama")
+                .kullan();
 //                .paraflaKontrol();
 
         evrakOlusturPage
@@ -165,7 +167,17 @@ public class AntetIslemleri extends BaseTest {
         evrakOlusturPage
                 .editorTabKontrol()
                 .editorKonuKontrol(konu)
-                .editorAntetKontrol("T.C. \n ANKARA", "Normal Antet", "Üst Birim Normal Antet");
+                .editorAntetKontrol("Normal Antet", "Üst Birim Normal Antet", "ANKARA");
+
+        evrakOlusturPage
+                .ekleriTabAc()
+                .ekListesiKontrol("EK-1", "Dağıtım Listesi")
+                .dosyaEkle(pathToFileText,fileName)
+                .ekleriEkMetniDoldur(konu)
+                .ekleriEkle()
+                .ekListesiKontrol("EK-2", fileName)
+                .ekListesiniEkYap()
+                .ekListesiKontrol("EK-3","Ek Listesi");
 //                .editorHitapKontrol(geregiKurum.toUpperCase());
 //                .editorImzaciKontrol(user2)
 //                .editorImzaciKontrol(user3)
