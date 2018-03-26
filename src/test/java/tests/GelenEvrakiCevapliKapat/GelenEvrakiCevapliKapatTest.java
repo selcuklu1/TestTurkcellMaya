35995eaa84de1c56c441eb58acfa33b56667ad88
emre sencan <emre.sencan@optiim.com>
2018-03-26 16:44:11 +0300

    commit

--- src/test/java/tests/GelenEvrakiCevapliKapat/GelenEvrakiCevapliKapatTest.java
+++ src/test/java/tests/GelenEvrakiCevapliKapat/GelenEvrakiCevapliKapatTest.java
@@ -921,7 +921,7 @@ public class GelenEvrakiCevapliKapatTest extends BaseTest {
         String ad = "Test";
         String soyad = "Otomasyon";
         String kisiKurum = "Gerçek Kişi";
-        String basariMesaji = "İşlem başarılıdır!";
+        String basariMesaji = "İşlem Başarılıdır!";
         String tur = "IMZALAMA";
         String kaldirilacakKlasor = "Diğer";
         String kladirilacakKlasorTitle = "[Klasör] 000";
