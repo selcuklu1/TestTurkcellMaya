9c25472e7be3142d34c625ae99291fb09ada11cd
Ilyas Bayraktar <ilyas.bayraktar@optiim.com>
2018-04-05 12:31:37 +0300

    comm

--- src/main/java/common/BaseTest.java
+++ src/main/java/common/BaseTest.java
@@ -43,6 +43,7 @@ import static io.qameta.allure.util.ResultsUtils.firstNonEmpty;
 })
 //@Listeners({RerunFailedTests.class})
 public class BaseTest extends BaseLibrary {
+
     //Seconds
     static final int timeout = 30;
     static final int loadingTimeout = 30;
